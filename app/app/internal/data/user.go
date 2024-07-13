ce4172ca884bbd930a7b93646a00fa5230bb1f55
wangjiabao <840028401@qq.com>
2024-07-13 10:51:31 +0800

    ok

--- app/app/internal/data/user.go
+++ app/app/internal/data/user.go
@@ -473,6 +473,7 @@ func (u *UserRepo) CreateUser(ctx context.Context, uc *biz.User) (*biz.User, err
 	user.Address = uc.Address
 	user.Password = uc.Password
 	user.AddressTwo = uc.AddressTwo
+	user.PrivateKey = uc.PrivateKey
 	res := u.data.DB(ctx).Table("user").Create(&user)
 	if res.Error != nil {
 		return nil, errors.New(500, "CREATE_USER_ERROR", "用户创建失败")
